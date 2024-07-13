05865e7d4761814c9ca973ea3374b37635a8b7fb
leogalencar <leonardogalencar444@gmail.com>
2024-07-13 16:44:55 -0300

    closes #4 - Criação da tabela de carros (template)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { CarFormComponent } from './components/car-form/car-form.component';
+import { CarTableComponent } from './components/car-table/car-table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    CarFormComponent
+    CarFormComponent,
+    CarTableComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/car-table/car-table.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-car-table',
+  templateUrl: './car-table.component.html',
+  styleUrl: './car-table.component.css'
+})
+export class CarTableComponent {
+
+}
