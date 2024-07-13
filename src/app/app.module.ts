edbf9c5d9a0ef2ab3193bbf81a15e62a8a90459a
leogalencar <leonardogalencar444@gmail.com>
2024-07-13 16:47:19 -0300

    closes #10 - Componente Cars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './components/header/header.component';
 import { CarFormComponent } from './components/car-form/car-form.component';
 import { CarTableComponent } from './components/car-table/car-table.component';
+import { CarsComponent } from './pages/cars/cars.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     CarFormComponent,
-    CarTableComponent
+    CarTableComponent,
+    CarsComponent
   ],
   imports: [
     BrowserModule,
